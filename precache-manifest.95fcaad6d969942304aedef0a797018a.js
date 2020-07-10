a6280cf2d92ab2a7e9c55ecda5d837100b6a2612
anomam <marc.abouanoma@sunpowercorp.com>
2020-07-10 23:45:55 +0900

    Updates

---
+++ precache-manifest.95fcaad6d969942304aedef0a797018a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ad1f82505dd04db38f874705197fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e8a39fcc4915cce955a",
+    "url": "/static/js/2.df39e491.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.df39e491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067adb3be368967aedea",
+    "url": "/static/js/main.c35a9f4d.chunk.js"
+  },
+  {
+    "revision": "65b48e0240a65107eed9",
+    "url": "/static/js/runtime-main.d5eae445.js"
+  }
+]);
